90d4b09e43bcb9c66ac03442d9145af941724f8f
Flynn <flynnp4yne@gmail.com>
2023-09-18 11:03:03 +1200

    styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,6 @@ const Home = () => {
     const home = config.home;
     return (
         <div name='home' className='w-full h-screen bg-primary-color text-text-color dark:bg-dark-primary-color dark:text-dark-text-color'>
-            
-            {/* Container */}
             <div className='max-w-[1000px] mx-auto px-4 sm:px-8 flex flex-col justify-center h-full'>
                 <h1 className='text-4xl mb-4 sm:text-7xl font-bold'>{home.title}</h1>
                 <h2 className='text-4xl sm:text-3xl font-bold'>{home.tagline}</h2>
@@ -14,9 +12,7 @@ const Home = () => {
                     <button className='border-2 rounded-md px-6 py-3 my-5 flex items-center hover:bg-highlight-color hover:text-color hover:border-highlight-color dark:hover:bg-dark-highlight-color dark:hover:dark-text-color dark:hover:border-dark-highlight-color'>{home.button}</button>
                 </div>
             </div>
-
         </div>
     )
 }
-
 export default Home
