b1a4dcad678191cff1f51486a405f01b4c7c8d76
Flynn <flynnp4yne@gmail.com>
2023-09-18 13:37:29 +1200

    Fixing mistakes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,9 +44,9 @@ const Navbar = () => {
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-primary-color dark:bg-dark-primary-color flex flex-col justify-center items-center'}>
             <li className='py-6 text-4xl'><Link onClick={handleClick} to="home" smooth={true} duration={500}>Home</Link></li>
             <li className='py-6 text-4xl'><Link onClick={handleClick} to="about" smooth={true} duration={500}>About</Link></li>
-            <li className='py-6 text-4xl'><Link onClick={handleClick} to="home" smooth={true} duration={500}>Skills</Link></li>
-            <li className='py-6 text-4xl'><Link onClick={handleClick} to="home" smooth={true} duration={500}>Projects</Link></li>
-            <li className='py-6 text-4xl'><Link onClick={handleClick} to="home" smooth={true} duration={500}>Contact</Link></li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to="skills" smooth={true} duration={500}>Skills</Link></li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to="projects" smooth={true} duration={500}>Projects</Link></li>
+            <li className='py-6 text-4xl'><Link onClick={handleClick} to="contact" smooth={true} duration={500}>Contact</Link></li>
         </ul>
 
         {/* Social Icons */}
@@ -57,12 +57,6 @@ const Navbar = () => {
                 <li className='w-[160px] h-[60px] flex justify-between ml-[-100px] hover:ml-[-10px] duration-300 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-br-md'><a className='flex justify-between items-center w-full text-white' target='_blank' rel="noreferrer" href={social.instagram}>Instagram <FaInstagram size={30}/></a></li>
             </ul>
         </div>
-
-        {/* <div className='flex fixed flex-col top-[90%] right-0'>
-            <ul>
-                <li><Link to="home" smooth={true} duration={500}>Back to top</Link></li>
-            </ul>
-        </div> */}
     </div>
     )
 }
