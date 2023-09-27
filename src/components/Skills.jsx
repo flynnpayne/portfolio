c37aee4ebc2289e1dbcc516be0b6ba1d879e4142
Flynn <flynnp4yne@gmail.com>
2023-09-27 21:55:40 +1300

    Adding content

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import config from '../index.json'
+import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+import JavaScript from '../assets/javascript.png'
+import ReactImg from '../assets/react.png'
+import Tailwind from '../assets/tailwind.png'
 
 const Skills = () => {
     const skills = config.skills;
@@ -8,6 +13,29 @@ const Skills = () => {
             <div className='max-w-[1000px] mx-auto px-4 sm:px-8 flex flex-col justify-center h-full'>
                 <div className='mb-4'>
                     <h1 className='text-4xl inline font-bold border-b-4 border-highlight-color dark:border-dark-highlight-color'>{skills.title}</h1>
+                </div>
+
+                <div className='w-full flex justify-center flex-wrap text-center py-8'>
+                    <div className='bg-secondary-color dark:bg-dark-secondary-color rounded-md shadow-lg mt-4 w-full sm:hover:scale-105 duration-500 sm:m-4 sm:w-[15rem]'>
+                        <img className='w-20 mx-auto mt-4' src={HTML} alt="HTML icon" />
+                        <p className='my-4'>HTML</p>
+                    </div>
+                    <div className='bg-secondary-color dark:bg-dark-secondary-color rounded-md shadow-lg mt-4 w-full sm:hover:scale-105 duration-500 sm:m-4 sm:w-[15rem]'>
+                        <img className='w-20 mx-auto mt-4' src={CSS} alt="CSS icon" />
+                        <p className='my-4'>CSS</p>
+                    </div>
+                    <div className='bg-secondary-color dark:bg-dark-secondary-color rounded-md shadow-lg mt-4 w-full sm:hover:scale-105 duration-500 sm:m-4 sm:w-[15rem]'>
+                        <img className='w-20 mx-auto mt-4' src={JavaScript} alt="Javascript icon" />
+                        <p className='my-4'>JAVASCRIPT</p>
+                    </div>
+                    <div className='bg-secondary-color dark:bg-dark-secondary-color rounded-md shadow-lg mt-4 w-full sm:hover:scale-105 duration-500 sm:m-4 sm:w-[15rem]'>
+                        <img className='w-20 mx-auto mt-4' src={ReactImg} alt="React icon" />
+                        <p className='my-4'>REACT</p>
+                    </div>
+                    <div className='bg-secondary-color dark:bg-dark-secondary-color rounded-md shadow-lg mt-4 w-full sm:hover:scale-105 duration-500 sm:m-4 sm:w-[15rem]'>
+                        <img className='w-20 mx-auto mt-4' src={Tailwind} alt="Tailwind icon" />
+                        <p className='my-4'>TAILWIND</p>
+                    </div>
                 </div>
             </div>
         </div>
